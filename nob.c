57b70d12cb9815fa021519954f1ef6fc14488512
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-11-01 13:19:59 +0100

    .

--- nob.c
+++ nob.c
@@ -6,132 +6,144 @@
 #include "flag.h"
 
 typedef struct {
-  char **items;
-  int capacity;
-  int count;
+    char **items;
+    int    capacity;
+    int    count;
 } MY_Flags_args;
 
-Cmd cmd = {0};
+Cmd           cmd            = { 0 };
 
-MY_Flags_args ARGS = {0};
+MY_Flags_args ARGS           = { 0 };
 
-char *BUILD_DIR = "./build";
-char *SRC_DIR = "./src";
-char *THIRDPARTY_DIR = "./thirdparty";
-char *BUILD_MAIN;
+char         *BUILD_DIR      = "./build";
+char         *SRC_DIR        = "./src";
+char         *THIRDPARTY_DIR = "./thirdparty";
+char         *BUILD_MAIN;
 
-void cmd_cc() {
+void          cmd_cc( ) {
 #ifdef _WIN32
-  cmd_append(&cmd, "clang++");
+  cmd_append( &cmd, "clang++" );
 #else
-  cmd_append(&cmd, "g++");
+  cmd_append( &cmd, "g++" );
 #endif
 
-  cmd_append(&cmd, "-std=c++23", "-Wall", "-Wextra", "-Wpedantic", "-Werror", "-lm");
+  cmd_append( &cmd, "-std=c++23", "-Wall", "-Wextra", "-Werror", "-lm" );
 }
 
-void usage(FILE *stream) {
-  fprintf(stream, "Usage: ./%s [OPTIONS]\n", flag_program_name());
-  fprintf(stream, "OPTIONS:\n");
-  flag_print_options(stream);
+void usage( FILE *stream ) {
+  fprintf( stream, "Usage: ./%s [OPTIONS]\n", flag_program_name( ) );
+  fprintf( stream, "OPTIONS:\n" );
+  flag_print_options( stream );
 }
 
-void build_raylib() {
-  cmd_append(&cmd, "make", "-C",  temp_sprintf("%s/raylib/src", THIRDPARTY_DIR), "PLATFORM=PLATFORM_DESKTOP_GLFW", "RAYLIB_LIBTYPE=STATIC", temp_sprintf("RAYLIB_RELEASE_PATH=../../../%s", BUILD_DIR));
-  if (!cmd_run(&cmd))
-    exit(1);
+void build_raylib( ) {
+  cmd_append(
+    &cmd, "make", "-C", temp_sprintf( "%s/raylib/src", THIRDPARTY_DIR ),
+    "PLATFORM=PLATFORM_DESKTOP_GLFW", "RAYLIB_LIBTYPE=STATIC",
+    temp_sprintf( "RAYLIB_RELEASE_PATH=../../../%s", BUILD_DIR ) );
+  if ( !cmd_run( &cmd ) ) {
+    exit( 1 );
+  }
   cmd.count = 0;
 }
 
-int main(int argc, char **argv) {
+int main( int argc, char **argv ) {
 
-  da_append_many(&ARGS, argv, argc);
+  da_append_many( &ARGS, argv, argc );
 
-  NOB_GO_REBUILD_URSELF(argc, argv);
+  NOB_GO_REBUILD_URSELF( argc, argv );
 
-  bool help = false;
+  bool help  = false;
 
   bool debug = false;
 
-  bool run = false;
+  bool run   = false;
 
   bool build = false;
 
   bool clean = false;
 
-  flag_bool_var(&help, "-help", false, "Print this help");
-  flag_bool_var(&help, "h", false, "Print this help");
-  flag_bool_var(&run, "r", false, "Run build");
-  flag_bool_var(&run, "-run", false, "Run build");
-  flag_bool_var(&build, "b", false, "Build");
-  flag_bool_var(&build, "-build", false, "Build");
-  flag_bool_var(&debug, "d", false, "enable debug mode");
-  flag_bool_var(&debug, "-debug", false, "enable debug mode");
-  flag_bool_var(&clean, "-clean", false, "Clean build directory");
+  flag_bool_var( &help, "-help", false, "Print this help" );
+  flag_bool_var( &help, "h", false, "Print this help" );
+  flag_bool_var( &run, "r", false, "Run build" );
+  flag_bool_var( &run, "-run", false, "Run build" );
+  flag_bool_var( &build, "b", false, "Build" );
+  flag_bool_var( &build, "-build", false, "Build" );
+  flag_bool_var( &debug, "d", false, "enable debug mode" );
+  flag_bool_var( &debug, "-debug", false, "enable debug mode" );
+  flag_bool_var( &clean, "-clean", false, "Clean build directory" );
 
-  BUILD_MAIN = nob_temp_sprintf("%s/cbps-lwp", BUILD_DIR);
+  BUILD_MAIN = nob_temp_sprintf( "%s/cbps-lwp", BUILD_DIR );
 
-  if (!flag_parse(ARGS.count, ARGS.items)) {
-    usage(stderr);
-    flag_print_error(stderr);
-    exit(1);
+  if ( !flag_parse( ARGS.count, ARGS.items ) ) {
+    usage( stderr );
+    flag_print_error( stderr );
+    exit( 1 );
   }
 
-  argc = flag_rest_argc();
-  argv = flag_rest_argv();
+  argc = flag_rest_argc( );
+  argv = flag_rest_argv( );
 
-  if (help) {
-    usage(stdout);
-    exit(0);
+  if ( help ) {
+    usage( stdout );
+    exit( 0 );
   }
-  if (build) {
+  if ( build ) {
 
-    if (!nob_mkdir_if_not_exists(BUILD_DIR))
+    if ( !nob_mkdir_if_not_exists( BUILD_DIR ) ) {
       return 1;
-
-    build_raylib();
-
-    cmd_cc();
-    cmd_append(&cmd, "-o", BUILD_MAIN);
-    cmd_append(&cmd, temp_sprintf("-I%s", SRC_DIR));
-    cmd_append(&cmd, temp_sprintf("-L%s/", BUILD_DIR));
-    cmd_append(&cmd, "-I", temp_sprintf("%s/raylib/src", THIRDPARTY_DIR));
-    cmd_append(&cmd, "-I", temp_sprintf("%s/raylib/src/external", THIRDPARTY_DIR));
-    cmd_append(&cmd, "-L/opt/homebrew/opt/glfw/lib", "-I/opt/homebrew/opt/glfw/include");
-    if (debug) {
-      cmd_append(&cmd, "-ggdb", "-O0");
-    } else {
-      cmd_append(&cmd, "-O3");
     }
-    cmd_append(&cmd, temp_sprintf("%s/main.cpp", SRC_DIR));
-    cmd_append(&cmd, "-lraylib", "-lglfw");
+
+    build_raylib( );
+
+    cmd_cc( );
+    cmd_append( &cmd, "-o", BUILD_MAIN );
+    cmd_append( &cmd, temp_sprintf( "-I%s", SRC_DIR ) );
+    cmd_append( &cmd, temp_sprintf( "-L%s/", BUILD_DIR ) );
+    cmd_append( &cmd, "-I", temp_sprintf( "%s/raylib/src", THIRDPARTY_DIR ) );
+    cmd_append( &cmd, "-I", temp_sprintf( "%s/raygui/src", THIRDPARTY_DIR ) );
+    cmd_append(
+      &cmd, "-I", temp_sprintf( "%s/raylib/src/external", THIRDPARTY_DIR ) );
+    cmd_append(
+      &cmd, "-L/opt/homebrew/opt/glfw/lib",
+      "-I/opt/homebrew/opt/glfw/include" );
+    if ( debug ) {
+      cmd_append( &cmd, "-g", "-O0" );
+    } else {
+      cmd_append( &cmd, "-O3" );
+    }
+    cmd_append( &cmd, temp_sprintf( "%s/main.cpp", SRC_DIR ) );
+    cmd_append( &cmd, "-lraylib", "-lglfw" );
 #ifdef __APPLE__
-    cmd_append(&cmd, "-framework", "CoreFoundation");
-    cmd_append(&cmd, "-framework", "IOKit");
-    cmd_append(&cmd, "-framework", "AppKit");
-    cmd_append(&cmd, "-framework", "CoreGraphics");
-    cmd_append(&cmd, "-framework", "OpenGL");
+    cmd_append( &cmd, "-framework", "CoreFoundation" );
+    cmd_append( &cmd, "-framework", "IOKit" );
+    cmd_append( &cmd, "-framework", "AppKit" );
+    cmd_append( &cmd, "-framework", "CoreGraphics" );
+    cmd_append( &cmd, "-framework", "OpenGL" );
 #endif
-    if (!cmd_run(&cmd))
+    if ( !cmd_run( &cmd ) ) {
       return 1;
+    }
     cmd.count = 0;
   }
 
-  if (run) {
-    cmd_append(&cmd, BUILD_MAIN);
-    if (!cmd_run(&cmd))
+  if ( run ) {
+    cmd_append( &cmd, BUILD_MAIN );
+    if ( !cmd_run( &cmd ) ) {
       return 1;
+    }
     cmd.count = 0;
   }
 
-  if (clean) {
-    #ifdef _WIN32
-    cmd_append(&cmd, "rmdir", "/s", "/q", BUILD_DIR);
-    #else
-    cmd_append(&cmd, "rm", "-rf", BUILD_DIR);
-    #endif
-    if (!cmd_run(&cmd))
+  if ( clean ) {
+#ifdef _WIN32
+    cmd_append( &cmd, "rmdir", "/s", "/q", BUILD_DIR );
+#else
+    cmd_append( &cmd, "rm", "-rf", BUILD_DIR );
+#endif
+    if ( !cmd_run( &cmd ) ) {
       return 1;
+    }
     cmd.count = 0;
   }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 // std c++23
 #include <iostream>
+
 #include "../thirdparty/raylib/src/raylib.h"
 
 #define FLAG_IMPLEMENTATION
@@ -13,14 +14,14 @@
 
 namespace CBPS {
   void print_usage( FILE *stream = stdout ) {
-    std::fprintf( stream, "Usage: %s [Options]\n", flag_program_name() );
+    std::fprintf( stream, "Usage: %s [Options]\n", flag_program_name( ) );
     print_app_info( stdout );
     std::printf( "\nOptions:\n" );
     flag_print_options( stdout );
   }
-}
+}  // namespace CBPS
 
-#endif // CBPS_CONSTANTS_IMPLEMENTATION
+#endif  // CBPS_CONSTANTS_IMPLEMENTATION
 
 #ifdef NORD_COLORS_IMPLEMENTATION
 
@@ -28,37 +29,42 @@ namespace Nord {
   Color index_to_Color( NordColor color ) {
 
     return Color{
-      .r = static_cast< unsigned char >( ( index_to_uint32( color ) >> 24 ) & 0xFF ),
-      .g = static_cast< unsigned char >( ( index_to_uint32( color ) >> 16 ) & 0xFF ),
-      .b = static_cast< unsigned char >( ( index_to_uint32( color ) >> 8 ) & 0xFF ),
-      .a = static_cast< unsigned char >( index_to_uint32( color ) & 0xFF )
-    };
+      .r = static_cast< unsigned char >(
+        ( index_to_uint32( color ) >> 24 ) & 0xFF ),
+      .g = static_cast< unsigned char >(
+        ( index_to_uint32( color ) >> 16 ) & 0xFF ),
+      .b = static_cast< unsigned char >(
+        ( index_to_uint32( color ) >> 8 ) & 0xFF ),
+      .a = static_cast< unsigned char >( index_to_uint32( color ) & 0xFF ) };
   }
-} // namespace Nord
+}  // namespace Nord
 
-#endif // NORD_COLORS_IMPLEMENTATION
-
+#endif  // NORD_COLORS_IMPLEMENTATION
 
 int main( int argc, char *argv[] ) {
   // Parse command line flags using flag.h
-  bool show_version = false;
-  bool show_V_version = false;
-  bool show_help = false;
-  bool show_h_help = false;
-  char *title = (char *)CBPS::DEFAULT_TITLE;
-  char *sub_title = (char *)CBPS::DEFAULT_SUB_TITLE;
-  uint64_t window_width = CBPS::DEFAULT_WINDOW_WIDTH;
-  uint64_t window_height = CBPS::DEFAULT_WINDOW_HEIGHT;
-
+  bool     show_version   = false;
+  bool     show_V_version = false;
+  bool     show_help      = false;
+  bool     show_h_help    = false;
+  char    *title          = ( char * ) CBPS::DEFAULT_TITLE;
+  char    *sub_title      = ( char * ) CBPS::DEFAULT_SUB_TITLE;
+  uint64_t window_width   = CBPS::DEFAULT_WINDOW_WIDTH;
+  uint64_t window_height  = CBPS::DEFAULT_WINDOW_HEIGHT;
 
   flag_bool_var( &show_version, "-version", false, "Show version information" );
-  flag_bool_var( &show_V_version, "V", false, "Show version information (short)" );
+  flag_bool_var(
+    &show_V_version, "V", false, "Show version information (short)" );
   flag_bool_var( &show_help, "-help", false, "Show help information" );
   flag_bool_var( &show_h_help, "h", false, "Show help information (short)" );
   flag_str_var( &title, "-title", CBPS::DEFAULT_TITLE, "Set window title" );
-  flag_str_var( &sub_title, "-sub_title", CBPS::DEFAULT_SUB_TITLE, "Set window sub title" );
-  flag_uint64_var( &window_width, "-width", CBPS::DEFAULT_WINDOW_WIDTH, "Set window width" );
-  flag_uint64_var( &window_height, "-height", CBPS::DEFAULT_WINDOW_HEIGHT, "Set window height" );
+  flag_str_var(
+    &sub_title, "-sub_title", CBPS::DEFAULT_SUB_TITLE, "Set window sub title" );
+  flag_uint64_var(
+    &window_width, "-width", CBPS::DEFAULT_WINDOW_WIDTH, "Set window width" );
+  flag_uint64_var(
+    &window_height, "-height", CBPS::DEFAULT_WINDOW_HEIGHT,
+    "Set window height" );
 
   if ( !flag_parse( argc, argv ) ) {
     flag_print_error( stderr );
@@ -67,7 +73,7 @@ int main( int argc, char *argv[] ) {
   }
 
   if ( show_version || show_V_version ) {
-    CBPS::print_app_version();
+    CBPS::print_app_version( );
     return 0;
   }
 
@@ -77,21 +83,30 @@ int main( int argc, char *argv[] ) {
   }
 
   // Initialize raylib window
-  SetConfigFlags( /*FLAG_WINDOW_UNDECORATED |*/ FLAG_WINDOW_RESIZABLE /*| FLAG_WINDOW_MOUSE_PASSTHROUGH */ | FLAG_WINDOW_HIGHDPI | FLAG_MSAA_4X_HINT | FLAG_INTERLACED_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_ALWAYS_RUN | FLAG_WINDOW_MAXIMIZED | FLAG_WINDOW_UNFOCUSED );
+  SetConfigFlags(                       /*FLAG_WINDOW_UNDECORATED |*/
+                  FLAG_WINDOW_RESIZABLE /*|
+                                           FLAG_WINDOW_MOUSE_PASSTHROUGH
+                                         */
+                  | FLAG_WINDOW_HIGHDPI | FLAG_MSAA_4X_HINT
+                  | FLAG_INTERLACED_HINT | FLAG_VSYNC_HINT
+                  | FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_ALWAYS_RUN
+                  | FLAG_WINDOW_MAXIMIZED | FLAG_WINDOW_UNFOCUSED );
   InitWindow( window_width, window_height, CBPS::APP_NAME );
   SetWindowMinSize( CBPS::MIN_WINDOW_WIDTH, CBPS::MIN_WINDOW_HEIGHT );
-  SetTargetFPS(60);
-  while ( !WindowShouldClose() ) {
-    BeginDrawing();
-    ClearBackground( index_to_Color(Nord::NORD0) );
+  SetTargetFPS( 60 );
+  while ( !WindowShouldClose( ) ) {
+    BeginDrawing( );
+    ClearBackground( index_to_Color( Nord::NORD0 ) );
 
-    DrawText( title, 10, 10, 20, index_to_Color(Nord::NORD5) );
-    DrawText( sub_title, 10, 40, 15, index_to_Color(Nord::NORD5) );
+    DrawText( title, 10, 10, 20, index_to_Color( Nord::NORD5 ) );
+    DrawText( sub_title, 10, 40, 15, index_to_Color( Nord::NORD5 ) );
 
-    EndDrawing();
+    
+
+    EndDrawing( );
   }
 
-  CloseWindow();
+  CloseWindow( );
 
   return 0;
 }
