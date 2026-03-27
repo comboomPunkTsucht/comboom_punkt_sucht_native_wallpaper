680799353be0eb92c7b40b7a76aeb8f72e9257f9
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2026-03-27 13:29:10 +0100

    feat: füge svelte-preprocess hinzu und konfiguriere den Service Worker

---
+++ WASM/svelte.config.ts
@@ -0,0 +1,31 @@
+import adapter from "@sveltejs/adapter-vercel";
+import { type Config } from "@sveltejs/kit";
+import { sveltePreprocess } from "svelte-preprocess";
+
+const config: Config = {
+	kit: {
+		adapter: adapter(),
+		serviceWorker: {
+			register: true,
+			options: {
+				type: "module",
+				updateViaCache: "all",
+			},
+		},
+	},
+	preprocess: [
+		sveltePreprocess({
+			typescript: true,
+		}),
+	],
+	vitePlugin: {
+		inspector: true,
+	},
+	compilerOptions: {
+		dev: process.env.NODE_ENV === "development",
+		hmr: process.env.NODE_ENV === "development",
+	},
+	prebundleSvelteLibraries: true,
+};
+
+export default config;
